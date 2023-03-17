23e3bd271ac83b2ffbacc49f5b2e72e07d3e233c
agustinhaag <agustin-haag@hotmail.com>
2023-03-17 16:56:47 -0300

    Primer versión

---
+++ Js/main.js
@@ -0,0 +1,39 @@
+let fila= document.querySelector(".contenedor-carrousel");
+let peliculas= document.getElementsByClassName("peliculas");
+let flechaizquierda=document.getElementById("flecha-izquierda")
+let flechaderecha=document.getElementById("flecha-derecha");
+
+flechaderecha.addEventListener("click", ()=>{
+    fila.scrollLeft += fila.offsetWidth ;
+     let indicadoractivo= document.querySelector(".indicadores .activo");
+if(indicadoractivo.nextSibling){
+    indicadoractivo.nextSibling.classList.add("activo");
+    indicadoractivo.classList.remove("activo");
+}
+})
+
+flechaizquierda.addEventListener("click", ()=>{
+    fila.scrollLeft -= fila.offsetWidth;
+    let indicadoractivo= document.querySelector(".indicadores .activo");
+if(indicadoractivo.previousSibling){
+    indicadoractivo.previousSibling.classList.add("activo");
+    indicadoractivo.classList.remove("activo");
+}
+})
+
+const numpag= Math.ceil(peliculas.length / 5);
+for(let i=0; i<numpag ;i++){
+    const indicador= document.createElement('button');  
+
+if(i===0){
+    indicador.classList.add("activo");
+}    document.querySelector('.indicadores').appendChild(indicador);
+indicador.addEventListener("click", (e)=>{
+    fila.scrollLeft=i*fila.offsetWidth;
+    document.querySelector(".indicadores .activo").classList.remove("activo");
+    e.target.classList.add("activo");
+})
+}
+ 
+
+
