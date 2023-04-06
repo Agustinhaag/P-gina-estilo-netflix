987cf7b69fe27818fb232f555746c6e3492dd31b
agustinhaag <agustin-haag@hotmail.com>
2023-04-05 22:54:54 -0300

    actualización de menu responsive

--- Js/main.js
+++ Js/main.js
@@ -35,5 +35,23 @@ indicador.addEventListener("click", (e)=>{
 })
 }
  
+let menu = document.getElementById("menu");
+let mostrar = document.getElementById("mostrar");
+let ocultar = document.getElementById("ocultar");
+let enlaces= document.querySelectorAll('.menu a[href^="#"]');
+mostrar.addEventListener("click",()=>{
+ menu.classList.add("visible");
+})
+ocultar.addEventListener("click",()=>{
+    menu.classList.remove("visible");
+   })
+
+enlaces.forEach(enlace=> {
+    enlace.addEventListener("click",()=>{
+  menu.classList.remove("visible");
+   });
+
+})
+   
 
 
